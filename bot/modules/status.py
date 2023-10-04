922b8941f29ae50719752004030b7fab2db05e76
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-10-04 22:53:36 +0530

    Update status.py

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -18,7 +18,7 @@ async def mirror_status(client, message):
     if count == 0:
         currentTime = get_readable_time(time() - botStartTime)
         free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
-        msg = f"<u><b>Pᴏᴡᴇʀᴇᴅ ʙʏ ᴜʟᴛʀᴏɴ ᴍʟᴛʙ...⚡️</b></u>\n"
+        msg = f"<u><b>Pᴏᴡᴇʀᴇᴅ ʙʏ ᴜʟᴛʀᴏɴ ᴍʟᴛʙ...⚡️</b></u>\n\n"
         msg += 'No Active Downloads !\n___________________________'
         msg += f"\n<b>Bot UpTime</b>: {currentTime}"
         msg += f"\n<b>Disk Free</b>: {free}"
